2151f7cdf9ebeba5f567c2ecadd4e0ff4673e772
Sooophy <eggtart11@163.com>
2022-01-12 16:33:13 -0500

    Analyze HDL test result

--- blood_calculator.py
+++ blood_calculator.py
@@ -9,4 +9,15 @@ def interface():
             keep_running = False
     return
 
-interface()
+def accept_input(test_name):
+    entry = input("Enter the {} test result: ".format(test_name))
+    return int(entry))
+
+def check_HDL(HDL_value):
+    if HDL_value >=60:
+        answer = "Normal"
+    elif 60>HDL_value >=40:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
