0d36af96586461172fdd5713d585a10aa38d6f56
Sooophy <eggtart11@163.com>
2022-01-12 18:33:14 -0500

    Wrote function to receive generic test input

--- blood_calculator.py
+++ blood_calculator.py
@@ -6,18 +6,13 @@ def interface():
         print("9-Quit")
         choice = input("Enter your choice:")
         if choice == "9":
-            keep_running = False
-    return
+            keep_running = False 
+
+        return
 
 def accept_input(test_name):
     entry = input("Enter the {} test result: ".format(test_name))
-    return int(entry))
+    return int(entry)
 
-def check_HDL(HDL_value):
-    if HDL_value >=60:
-        answer = "Normal"
-    elif 60>HDL_value >=40:
-        answer = "Borderline Low"
-    else:
-        answer = "Low"
-    return answer
+
+interface()
