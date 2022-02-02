653c8e3bac78c18b61c0caf0bbf7bf5378b64e32
Sooophy <eggtart11@163.com>
2022-02-02 16:22:42 -0500

    modified testing and initiated gihub actions, modified weight_entry files

--- test_weight_entry.py
+++ test_weight_entry.py
@@ -1,9 +1,15 @@
 import pytest
 
 
+# include all possible input, and write test
 @pytest.mark.parametrize("input, expected", [
     ("22 lb", 10),
     ("50 kg", 50),
+    ("22 lbs", 10),
+    # ("22lb", 10),
+    ("22 LB", 10),
+    ("-22 lb", -10),
+    # ("ten kg", 10),
     ])
 def test_parse_weight_input(input, expected):
     from weight_entry import parse_weight_input
